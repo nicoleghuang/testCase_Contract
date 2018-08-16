fc0961aba8ea697ae15a3f4f75e8ea0d0698c656
nicoleghuang <1107910050@qq.com>
2018-08-16 15:07:31 +0800

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,7 +1,7 @@
 #coding=utf-8
 def diction():
-    #d={'15815563126':'123456','13250791512':'123456'}
-    d={'15815563126':'123456'}
+    d={'username1':'pw1','usename2':'pw1'}
+    
     print "Read username and password successfully!"
     return d
 
